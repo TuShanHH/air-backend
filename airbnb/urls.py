378b973fe1bdc4d72900c183248449aed5aebba7
tushan-honghong <tsm623227952@outlok.com>
2025-09-03 11:27:08 +0800

    update

--- airbnb/urls.py
+++ airbnb/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('hello/', hello, name='hello'),
     path('register/', register, name='register'),
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-
-    path('token/ref resh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
